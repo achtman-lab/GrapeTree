4ce4b0eede7ef846f98bd361f5a8123f05652e17
Zhemin Zhou <zhemin.zhou@warwick.ac.uk>
2017-05-17 19:21:12 +0100

    Update documentation

--- MSTrees.py
+++ MSTrees.py
@@ -256,7 +256,35 @@ def nonredundent(names, profiles) :
     return names, profiles, embeded
 
 def backend(**parameters) :
-    # method: PSA, eBurst, ninja
+    '''
+    paramters :
+        profile: input file. Can be either profile or fasta. Headings start with an '#' will be ignored. 
+        method: MST or NJ
+        matrix_type: asymmetric or symmetric
+        edge_weight: harmonic or eBurst
+        neighbor_branch_reconnection: T or F
+    
+    Outputs :
+        A string of a NEWICK tree
+    
+    Examples :
+        To run a Balanced Spanning Arborescence (BSA), use :
+        backend(profile=<filename>, method='MST', matrix_type='asymmetric', edge_weight='harmonic', neighbor_branch_reconnection='T')
+    
+        OR simply
+        backend(profile=<filename>)
+        
+        To run a standard minimum spanning tree :
+        backend(profile=<filename>, method='MST', matrix_type='symmetric', edge_weight='eBurst', neighbor_branch_reconnection='F')
+        
+        To run a NJ tree (using FastME 2.0) :
+        backend(profile=<filename>, method='NJ')
+    
+    Can also be called in command line:
+        BSA: MSTrees.py profile=<filename> method=MST matrix_type=asymmetric edge_weight=harmonic neighbor_branch_reconnection=T
+        MST: MSTrees.py profile=<filename> method=MST matrix_type=symmetric edge_weight=eBurst neighbor_branch_reconnection=F
+        NJ:  MSTrees.py profile=<filename> method=NJ
+    '''
     params.update(parameters)
 
     names = []
@@ -299,8 +327,8 @@ def backend(**parameters) :
         node.add_child(n)
         n.edge_length = 0.0
     
-    return tre
+    return tre.as_string('newick')
 
 if __name__ == '__main__' :
     tre = backend(**dict([p.split('=') for p in sys.argv[1:]]))
-    print tre.as_string('newick')
+    print tre
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # EnteroMSTree
+
+
+## MSTrees.py
+### paramters :
+        profile: input file. Can be either profile or fasta. Headings start with an '#' will be ignored. 
+        method: MST or NJ
+        matrix_type: asymmetric or symmetric
+        edge_weight: harmonic or eBurst
+        neighbor_branch_reconnection: T or F
+    
+### Outputs :
+        A string of a NEWICK tree
+    
+### Examples :
+        To run a Balanced Spanning Arborescence (BSA), use :
+        backend(profile=<filename>, method='MST', matrix_type='asymmetric', edge_weight='harmonic', neighbor_branch_reconnection='T')
+    
+        OR simply
+        backend(profile=<filename>)
+        
+        To run a standard minimum spanning tree :
+        backend(profile=<filename>, method='MST', matrix_type='symmetric', edge_weight='eBurst', neighbor_branch_reconnection='F')
+        
+        To run a NJ tree (using FastME 2.0) :
+        backend(profile=<filename>, method='NJ')
+    
+### Can also be called in command line:
+        BSA: MSTrees.py profile=<filename> method=MST matrix_type=asymmetric edge_weight=harmonic neighbor_branch_reconnection=T
+        MST: MSTrees.py profile=<filename> method=MST matrix_type=symmetric edge_weight=eBurst neighbor_branch_reconnection=F
+        NJ:  MSTrees.py profile=<filename> method=NJ
