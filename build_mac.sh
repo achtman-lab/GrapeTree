498364a3923f401cff77541d574c292836ea3ccc
Nabil Alikhan <Nabil Alikhan>
2017-07-04 16:47:02 +0100

    MACOSX is now build an app

--- build_mac.sh
+++ build_mac.sh
@@ -1,4 +1,4 @@
-pyinstaller -n GrapeTree_mac --onefile --icon=GT_icon.icns \
+pyinstaller -n GrapeTree_mac --windowed --icon=GT_icon.icns \
     --add-binary binaries/edmonds-osx:binaries/ \
     --add-binary binaries/fastme-2.1.5-osx:binaries/ \
     --add-data grapetree/templates/:grapetree/templates/  \
