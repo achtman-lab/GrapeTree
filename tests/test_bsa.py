77ae2f8fff9760d3aae795ef37add038cc3fbca6
Nabil Alikhan <nabil@happykhan.com>
2018-04-17 16:11:27 +0100

    index

--- tests/test_bsa.py
+++ tests/test_bsa.py
@@ -1,33 +1,25 @@
-import sys
-sys.path.append('c:\\Users\\Nabil\\Dropbox\\py\\EnteroMSTree')
 from grapetree import app
 import pytest
 import os
 
 def test_bsa_asymetric(): 
+    print  os.getcwd()
     app_test = app.test_client()
-    with open(os.path.join('examples','sim14_005_0_7.profile')) as f:
+    with open(os.path.join('..', 'examples','simulated_data.profile')) as f:
         test_profile = f.read()
-    with open(os.path.join('examples','sim14_005_0_7.BSA.nwk')) as f:
+    with open(os.path.join('..', 'examples','simulated_data.BSA.nwk')) as f:
         test_results = f.read()
     tree =  app_test.post('/maketree', data=dict(profile=test_profile))
     assert str(tree.data).strip() == test_results
 
 def test_bsa_symetric(): 
     app_test = app.test_client()
-    with open(os.path.join('examples','sim14_005_0_7.profile')) as f:
+    with open(os.path.join('..', 'examples','simulated_data.profile')) as f:
         test_profile = f.read()
-    with open(os.path.join('examples','sim14_005_0_7.BSA_sym.nwk')) as f:
+    with open(os.path.join('..', 'examples','simulated_data.global.nwk')) as f:
         test_results = f.read()
     tree =  app_test.post('/maketree', data=dict(profile=test_profile, matrix_type='symmetric'))
     assert str(tree.data).strip() == test_results
-
-def test_bad_profile(): 
-    app_test = app.test_client()
-    with open(os.path.join('examples','bad_profile.profile')) as f:
-        test_profile = f.read()
-    tree =  app_test.post('/maketree', data=dict(profile=test_profile))
-    assert tree.status_code == 500
 
 def test_405():
     # maketree must be POST - cause of data size otherwise return 405
