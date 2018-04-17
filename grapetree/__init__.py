a479ae1c73753ef38a1c32cca798e20343b2cea1
Zhemin Zhou <zhemin.zhou@warwick.ac.uk>
2018-04-17 12:50:01 +0100

    move htmls out

--- grapetree/__init__.py
+++ grapetree/__init__.py
@@ -6,7 +6,7 @@ import os
 if getattr(sys, 'frozen', False):
     template_folder = os.path.join(sys._MEIPASS, 'grapetree','templates')
     static_folder = os.path.join(sys._MEIPASS, 'static')
-    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+    app = Flask(__name__, template_folder=os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))), static_folder=static_folder)
 else:
     app = Flask(__name__) # , static_folder='static/', template_folder='templates/')
 
