a5bc04acd96ffa8fad5e7b821907351db2da470b
happykhan <nabil@happykhan.com>
2018-07-05 17:33:17 +0100

    compiling 1.5

--- build_win.bat
+++ build_win.bat
@@ -1,4 +1,5 @@
 pyinstaller -n GrapeTree_win  --icon=GT_icon.ico ^
+    --add-binary binaries/rapidnj.exe;binaries/ ^
     --add-binary binaries/edmonds.exe;binaries/ ^
     --add-binary binaries/fastme.exe;binaries/ ^
     --add-data MSTree_holder.html;. ^
